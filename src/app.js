bfcb8bc7b1fa9357084081a1e006827ae76ac0bd
Valeria <waleria.shadrina@gmail.com>
2021-12-13 21:57:22 +0100

    Created search field and a function to read it's input on button click

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const baseURL = "https://api.genius.com/"
+
+const form = document.getElementById("form")
+
+const searchFieldInput = document.getElementById("searchField")
+console.log(searchFieldInput.value)
+
+const searchButton = document.getElementById("searchButton")
+
+let searchRequest;
+
+const readSearchInput = (event) => {
+  event.preventDefault()
+  console.log(searchFieldInput.value)
+  searchRequest = searchFieldInput.value;
+  form.reset()
+}
+
+
+searchButton.addEventListener("click", readSearchInput)
